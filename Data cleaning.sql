9f253045ef8b5b308ecc8f1ce18befa3e8401210
tracymutheu <117747057+tracymutheu@users.noreply.github.com>
2024-02-07 13:58:21 +0300

    Add files via upload

---
+++ Data cleaning.sql
@@ -0,0 +1,67 @@
+-- DATA CLEANING 
+
+SELECT * FROM projects.hr;
+
+USE projects;
+
+ALTER TABLE hr
+CHANGE COLUMN ï»¿id emp_id varchar(20) NULL;
+
+DESCRIBE hr;
+
+
+SELECT birthdate FROM hr;
+
+SET sql_safe_updates = 0;  /*change back to 1 since its security to protect the db*/
+
+UPDATE hr
+SET birthdate = CASE 
+	WHEN birthdate LIKE'%/%'THEN date_format(str_to_date(birthdate,'%m/%d/%Y'),'%Y-%m-%d')
+    WHEN birthdate LIKE '%-%'THEN date_format(str_to_date(birthdate,'%m-%d-%Y'),'%Y/%m/%d')
+    ELSE NULL
+END;
+
+ALTER TABLE hr
+MODIFY COLUMN birthdate DATE;
+SELECT birthdate FROM hr;
+
+UPDATE hr
+SET hire_date = CASE 
+	WHEN hire_date LIKE'%/%'THEN date_format(str_to_date(hire_date,'%m/%d/%Y'),'%Y-%m-%d')
+    WHEN hire_date LIKE '%-%'THEN date_format(str_to_date(hire_date,'%m-%d-%Y'),'%Y/%m/%d')
+    ELSE NULL
+END;
+
+ALTER TABLE hr
+MODIFY COLUMN hire_date DATE;
+SELECT hire_date FROM hr;
+
+UPDATE hr
+SET termdate = date(str_to_date(termdate,'%Y-%m-%d %H:%i:%s UTC'))
+WHERE termdate IS NOT NULL AND termdate !='';
+
+UPDATE hr
+SET termdate = if(termdate is NOT NULL AND termdate !='',date(str_to_date(termdate, '%Y-%m-%d %H:%i:%s UTC')), '0000-00-00')
+WHERE true;
+
+SELECT termdate FROM hr;
+
+SET sql_mode = 'ALLOW_INVALID_DATES';
+
+ALTER TABLE hr
+MODIFY COLUMN termdate DATE;
+SELECT termdate FROM hr;
+
+ALTER TABLE hr ADD COLUMN age int;
+
+UPDATE hr
+SET age = timestampdiff(YEAR, birthdate, CURDATE());
+
+SELECT birthdate, age FROM hr;
+
+SELECT 
+	min(age)AS youngest,
+    max(age)AS oldest
+FROM hr;
+
+SELECT count(*)FROM hr WHERE age < 18;
